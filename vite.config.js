929a77abb8ed9f7667ee375f297f30a4458af9f0
Fousiya N <fousiyafousy01@gmail.com>
2026-03-05 22:11:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: '/lost-item-tracker/',   // IMPORTANT for GitHub Pages
   build: {
     chunkSizeWarningLimit: 1600,
     rollupOptions: {
